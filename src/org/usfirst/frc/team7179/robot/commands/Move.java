724c6205d360ee04378990c143c1ea84c15d397d
bbarbee <brenda.barbee@orbitz.com>
2018-01-12 22:10:38 -0600

    Add DriveBase

---
+++ src/org/usfirst/frc/team7179/robot/commands/Move.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team7179.robot.commands;
+
+import org.usfirst.frc.team7179.robot.Robot;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Move extends Command {
+	public Move() {
+		requires(Robot.drive);
+	}
+	@Override
+	protected void initialize() {
+	}
+	public void execute() {
+		Robot.drive.move(Robot.OI.getLeftSpeed(), Robot.OI.getRightSpeed());
+	}
+	protected boolean isFinished() {
+		return false;
+	}
+	protected void end() {
+		Robot.drive.move(0, 0);
+	}
+	protected void interrupted() {
+		Robot.drive.move(0, 0);
+	}
+
+}
---
+++ src/org/usfirst/frc/team7179/robot/subsystems/DriveBase.java
@@ -0,0 +1,34 @@
+package org.usfirst.frc.team7179.robot.subsystems;
+
+import org.usfirst.frc.team7179.robot.RobotMap;
+import org.usfirst.frc.team7179.robot.commands.Move;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DriveBase extends Subsystem{
+	private TalonSRX leftMotor1, rightMotor1;
+	private VictorSPX leftMotor2, rightMotor2;
+	public DriveBase(){
+		//Create left motors
+		leftMotor1 = new TalonSRX(RobotMap.DRIVE_LEFT1);
+		leftMotor2 = new VictorSPX(RobotMap.DRIVE_LEFT2);
+		leftMotor2.follow(leftMotor1);
+		
+		//Create right motors
+		rightMotor1 = new TalonSRX(RobotMap.DRIVE_RIGHT1);
+		rightMotor2 = new VictorSPX(RobotMap.DRIVE_RIGHT2);
+		rightMotor2.follow(rightMotor1);
+	}
+	protected void initDefaultCommand(){
+		setDefaultCommand(new Move());
+	}
+	public void move(double left, double right) {
+		leftMotor1.set(ControlMode.PercentOutput, left);
+		rightMotor1.set(ControlMode.PercentOutput, -right);
+	}
+
+}
