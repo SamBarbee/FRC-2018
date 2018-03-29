4919080a35cf3f48892f67bd5db5ecf10b4da286
Sam Barbee <sambarbee@me.com>
2018-03-29 10:16:40 -0500

    2 cube tune

--- src/org/usfirst/frc/team7179/robot/commands/autonomous/Center2Cube.java
+++ src/org/usfirst/frc/team7179/robot/commands/autonomous/Center2Cube.java
@@ -29,39 +29,53 @@ public class Center2Cube extends DynamicAuton {
 			if(Robot.gameData.charAt(0) == 'L')
 			{
 				addParallel(new RunLift(0.3));
+				addSequential(new DriveArc(24,48,true));
+				addSequential(new DriveArc(60,120,false));
+				
+				addParallel(new RunLift(-0.5));
+				addSequential(new WaitCommand(0.1));
+				addParallel(new RunIntake(-1));
+				addSequential(new WaitCommand(0.3));
+				addParallel(new RunLift(-0.2));
+				addParallel(new RunIntake(0));
+				
+				addSequential(new DriveArc(-60,70,true));
+				addParallel(new RunIntake(1));
+				addSequential(new DriveTurn(-60,250,500));
+				addSequential(new DriveStraight(12,200,500));
+				
+				addSequential(new DriveArc(-30,80,true));
+				addSequential(new DriveTurn(-80,400,1000));
+				
+				addSequential(new DriveStraight(24,300,1000));
+				addParallel(new RunIntake(-1));
+				addSequential(new WaitCommand(0.3));
+				addParallel(new RunIntake(0));
+	
+			} else {
+				addParallel(new RunLift(0.3));
+				addSequential(new DriveArc(24,48,false));
 				addSequential(new DriveArc(60,120,true));
 				
 				addParallel(new RunLift(-0.5));
 				addSequential(new WaitCommand(0.1));
 				addParallel(new RunIntake(-1));
 				addSequential(new WaitCommand(0.3));
-				addParallel(new RunLift(-0.5));
+				addParallel(new RunLift(-0.2));
 				addParallel(new RunIntake(0));
 				
-				addSequential(new DriveStraight(-30,400,1000));
+				addSequential(new DriveArc(-60,70,false));
 				addParallel(new RunIntake(1));
-				addSequential(new DriveTurn(-60,250,500));
+				addSequential(new DriveTurn(60,250,500));
 				addSequential(new DriveStraight(12,200,500));
 				
-	
-			} else {
-				addParallel(new RunLift(0.3));
-				addSequential(new DriveTime(0.25,0.35));
-				addSequential(new WaitCommand(0.3));
-				addSequential(new TurnTime(0.25,0.35,-1));
-				addSequential(new WaitCommand(0.3));
-				addSequential(new DriveTime(2.4,0.35));
-				addSequential(new WaitCommand(0.3));
-				addSequential(new TurnTime(0.25,0.35,1));
-				addSequential(new WaitCommand(0.3));
-				addSequential(new DriveTime(1,0.4));
-				addSequential(new WaitCommand(0.5));
-				addParallel(new RunLift(-0.2));
-				addSequential(new WaitCommand(0.5));
+				addSequential(new DriveArc(-30,80,true));
+				addSequential(new DriveTurn(-80,400,1000));
+				
+				addSequential(new DriveStraight(24,300,1000));
 				addParallel(new RunIntake(-1));
 				addSequential(new WaitCommand(0.3));
 				addParallel(new RunIntake(0));
-				addSequential(new DriveTime(0.75,-0.25));
 			}
 		}	
 }
